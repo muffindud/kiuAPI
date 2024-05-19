c0f9a3b867d5ed615a96754751ffa26c86f194ec
Corneliu <73847818+muffindud@users.noreply.github.com>
2024-05-19 15:10:09 +0300

    Feat/data upload (#2)
    * create basic queue endpoint
    
    * create DataManager class
    
    * fix first file write
    
    * implement GET endpoints
    
    * implement POST endpoints
    
    * implement PUT endpoints
    
    * implement DELETE endpoints

---
+++ src/data_manager.py
@@ -0,0 +1,45 @@
+import json
+from os.path import isfile
+
+
+class DataManager:
+    instance = None
+    file_path = 'data/queues.json'
+
+
+    def __init__(self):
+        if not isfile(self.file_path):
+            with open(self.file_path, 'w') as file:
+                file.write('{}')
+
+        with open(self.file_path, 'r') as file:
+            self.data = json.load(file)
+
+
+    def get_data(self, keys=[]) -> dict:
+        if keys:
+            return {k: self.data[k] for k in keys if k in self.data}
+
+        return self.data
+
+
+    def add_data(self, data: dict, update: bool = False) -> None:
+        if any(k in self.data for k in data.keys()) and not update:
+            return -1
+
+        for key, value in data.items():
+            self.data[key] = value
+
+        with open(self.file_path, 'w') as file:
+            json.dump(self.data, file, indent=4)
+
+
+    def remove_data(self, key: list) -> None:
+        if any(k not in self.data for k in key):
+            return -1
+
+        for k in key:
+            self.data.pop(k)
+
+        with open(self.file_path, 'w') as file:
+            json.dump(self.data, file, indent=4)
--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,14 @@
+import json
+
 from flask import jsonify, request
 
 from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token, create_refresh_token
 
 from __main__ import app, env_values, jwt
+from src.data_manager import DataManager
+
+
+data_manager = DataManager()
 
 @app.route('/api/v1/token', methods=['POST'])
 def token():
@@ -42,3 +48,138 @@ def refresh():
         'access_token': create_access_token(identity=current_user),
         'permissions': current_user
     }), 200
+
+
+@app.route('/api/v1/queue', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@jwt_required()
+def queue():
+    current_user = get_jwt_identity()
+
+    try:
+        if request.method == 'GET':
+            if "R" in current_user:
+                response = {}
+
+                start_id = request.args.get('start_id')
+                end_id = request.args.get('end_id')
+                id = request.args.get('id')
+
+                if start_id and end_id and not id:
+                    start_id = int(start_id)
+                    response['start_id'] = start_id
+
+                    end_id = int(end_id)
+                    response['end_id'] = end_id
+
+                    if start_id > end_id:
+                        return jsonify({'msg': 'start_id should be smaller or equal than end_id'}), 400
+
+                    response = data_manager.get_data(keys=[str(i) for i in range(start_id, end_id + 1)])
+
+                elif id and not start_id and not end_id:
+                    id = json.loads(id)
+                    
+                    if type(id) is not list:
+                        id = [str(id)]
+                    else:
+                        id = [str(i) for i in id]
+
+                    response = data_manager.get_data(keys=id)
+
+                    if response == {}:
+                        return jsonify({'msg': 'No data found'}), 404
+
+                elif id and start_id and end_id:
+                    return jsonify({'msg': 'You should use either id or start_id and end_id'}), 400
+            
+                elif not id and not start_id and not end_id:
+                    response = data_manager.get_data()
+
+                return jsonify(response), 200
+            
+            else:
+                return jsonify({'msg': 'Unauthorized'}), 401
+
+        elif request.method == 'POST':
+            if "W" in current_user:
+                try:
+                    data = request.get_json()
+
+                    for key in data.keys(): 
+                        if "description" not in data[key] or "name" not in data[key] or "queue_list" not in data[key] or len(data[key].keys()) != 3:
+                            return jsonify({'msg': 'Bad Request'}), 400
+
+                    if data_manager.add_data(data) == -1:
+                        return jsonify({'msg': 'Data already exists'}), 409
+
+                    return jsonify({'msg': 'Data added'}), 201
+                except Exception as e:
+                    print(e)
+                    return jsonify({'msg': 'Bad Request'}), 400
+            else:
+                return jsonify({'msg': 'Unauthorized'}), 401
+
+        elif request.method == 'PUT':
+            if "U" in current_user:
+                try:
+                    data = request.get_json()
+
+                    for key in data.keys():
+                        if "description" not in data[key] or "name" not in data[key] or "queue_list" not in data[key] or len(data[key].keys()) != 3:
+                            return jsonify({'msg': 'Bad Request'}), 400
+
+                    data_manager.add_data(data, update=True)
+
+                    return jsonify({'msg': 'Data updated'}), 200
+                except Exception as e:
+                    print(e)
+                    return jsonify({'msg': 'Bad Request'}), 400
+            else:
+                return jsonify({'msg': 'Unauthorized'}), 401
+
+        elif request.method == 'DELETE':
+            if "D" in current_user:
+                try:
+                    start_id = request.args.get('start_id')
+                    end_id = request.args.get('end_id')
+                    id = request.args.get('id')
+
+                    if start_id and end_id and not id:
+                        start_id = int(start_id)
+                        end_id = int(end_id)
+
+                        if start_id > end_id:
+                            return jsonify({'msg': 'start_id should be smaller or equal than end_id'}), 400
+
+                        if data_manager.remove_data([str(i) for i in range(start_id, end_id + 1)]) == -1:
+                            return jsonify({'msg': 'Data not found'}), 404
+                        
+                    elif id and not start_id and not end_id:
+                        id = json.loads(id)
+
+                        if type(id) is not list:
+                            id = [str(id)]
+                        else:
+                            id = [str(i) for i in id]
+
+                        if data_manager.remove_data(id) == -1:
+                            return jsonify({'msg': 'Data not found'}), 404
+                    
+                    elif id and start_id and end_id:
+                        return jsonify({'msg': 'You should use either id or start_id and end_id'}), 400
+                    
+                    elif not id and not start_id and not end_id:
+                        return jsonify({'msg': 'You should use either id or start_id and end_id'}), 400
+                    
+                    return jsonify({'msg': 'Data removed'}), 200
+
+                except Exception as e:
+                    print(e)
+                    return jsonify({'msg': 'Bad Request'}), 400
+            else:
+                return jsonify({'msg': 'Unauthorized'}), 401
+
+    except Exception as e:
+        print(e)
+        print(data_manager)
+        return jsonify({'msg': 'Bad Request'}), 400
